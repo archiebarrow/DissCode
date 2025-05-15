78683f7af95e0839973f9bfd312f7bba509f9ad7
archiebarrow <ml21adb@leeds.ac.uk>
2025-05-15 14:17:25 +0100

    Update !DissertationModel!.R

--- !DissertationModel!.R
+++ !DissertationModel!.R
@@ -60,7 +60,7 @@ DebtPanelData <- read.csv("~/DISS/FinalDataFiles/DebtPanelData.csv")
 CollinearityCheck <- cor(DebtPanelData[, c("GenDebt", "GDPGrowth", "GenSpend", "Inflation", "GovRevenue", "PopGrowth", "UrbanPopGrowth", "Unemp", "AgeDepRatio", "HealthSpend", "MiliSpend", "EduSpend", "Corruption")])
 view(CollinearityCheck)
 
-#Correlation matrix with pairwise detection to combat missing data
+#Correlation matrix with pairwise deletion to combat missing data
 PairwiseCollinearity <- cor(DebtPanelData[, c("GenDebt", "GDPGrowth", "GenSpend", "Inflation", "GovRevenue", "PopGrowth", "UrbanPopGrowth", "Unemp", "AgeDepRatio", "HealthSpend", "MiliSpend", "EduSpend", "Corruption")], use = "pairwise.complete.obs")
 view(PairwiseCollinearity)
 
